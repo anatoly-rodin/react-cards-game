a7e751d16408f83fc21350e45e0f7abe57c5a51a
Anatolii Rodin <anatolyr1@gmail.com>
2020-05-06 15:01:20 +0300

    Updates

---
+++ precache-manifest.664c51958ed9180dc1bc633e3e645583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a14b9b9ead1bf6678ceb2118568ee47",
+    "url": "/react-card-game/index.html"
+  },
+  {
+    "revision": "3de98e7b448a7d1b4bf7",
+    "url": "/react-card-game/static/css/main.a01544e2.chunk.css"
+  },
+  {
+    "revision": "9b35345c19c46ef2a30b",
+    "url": "/react-card-game/static/js/2.cfca31f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-card-game/static/js/2.cfca31f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de98e7b448a7d1b4bf7",
+    "url": "/react-card-game/static/js/main.5ed6eea7.chunk.js"
+  },
+  {
+    "revision": "7b78432012e9a89b3392",
+    "url": "/react-card-game/static/js/runtime-main.d378cc1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-card-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
